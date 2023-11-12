0c57f12fc5cec8568bc2ef3cea21732c72fa8ab2
Andres Machado <andres@pop-os.localdomain>
2023-11-11 22:29:59 -0500

    Test prettier pre commit

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
+              src='/vercel.svg'
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
