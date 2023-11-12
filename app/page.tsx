3b87ad3e3144f7c3eee44daa58e1b4d9c2962dfe
Andres Machado <andres@pop-os.localdomain>
2023-11-11 22:19:35 -0500

    Test prettier pre commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
+        <ul><li>Pre commit test</li></ul>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
