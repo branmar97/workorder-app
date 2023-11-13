f8bfdf97bf4c38e1746be793c28c05610f008d23
amachad4 <amachad4@masonlive.gmu.edu>
2023-11-12 21:36:40 -0500

    Testing -o jest flag

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p data-testid='test-jest'>
-          Get started by editing&nbsp;
+          Get started b editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
